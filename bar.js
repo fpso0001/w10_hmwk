8a9a92a47e675ffc8bfd6e731ed258ba33ca5795
fpso0001 <89438934+fpso0001@users.noreply.github.com>
2021-10-10 18:24:05 +1100

    Add files via upload

---
+++ bar.js
@@ -0,0 +1,5 @@
+var vg_1 = "bar2.vg.json"; 
+
+vegaEmbed("#bar", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
